5dbf9cb0b92da3fb0d35c821407ea6aaf66555df
chukssomzzy <chukssomzzy19@gmail.com>
2022-12-16 23:42:18 +0100

    t

--- add_op.c
+++ add_op.c
@@ -12,7 +12,7 @@ void add_op(stack_t **t, unsigned int line_num)
 
 	tmp = *t;
 	if ((t && !(*t)) || !t)
-		cleanup(*t, line_num, "can,t add, stack too short");
+		cleanup(*t, line_num, "can't add, stack too short");
 	if ((tmp && !tmp->next) || !tmp)
 		cleanup(*t, line_num, "can't add, stack too short");
 	(*t)->next->n += (tmp->n);
