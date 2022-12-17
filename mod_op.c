d066d0b9c7662930db4dcfe118b83ce060740636
chukssomzzy <chukssomzzy19@gmail.com>
2022-12-17 17:54:16 +0100

    division by zero

--- mod_op.c
+++ mod_op.c
@@ -15,6 +15,8 @@ void mod_op(stack_t **t, unsigned int line_num)
 		cleanup(*t, line_num, "can't mod, stack too short");
 	if ((tmp && !tmp->next) || !tmp)
 		cleanup(*t, line_num, "can't mod, stack too short");
+	if (!tmp->n)
+		cleanup(*t, line_num, "division by zero");
 	(*t)->next->n %= tmp->n;
 	(*t)->next->prev = NULL;
 	*t = (*t)->next;
